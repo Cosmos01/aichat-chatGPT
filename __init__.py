5c885a904d029e41e7a0ab89531d73bc176bef5e
Cosmos <37209685+Cosmos01@users.noreply.github.com>
2023-03-04 17:33:53 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ import os
 
 class Config:
     _config = configparser.ConfigParser()
-    api_key: str = ""  # api key
+    api_keys: list = []  # api key
     model: str = "gpt-3.5-turbo"  # 模型
     record: bool = True  # 是否记录对话
     conversations: dict = {}  # 会话列表
@@ -15,7 +15,8 @@ class Config:
     def __init__(self):
         self._config.read(os.path.join(os.path.dirname(__file__), 'config.ini'), encoding='utf-8')
         self.load_conversations()
-        self.api_key = self._config.get("OPTION", "api_key", fallback="")
+        api_keys = self._config.get("OPTION", "api_key", fallback="")
+        self.api_keys = api_keys.split(",")
         self.model = self._config.get("OPTION", "model", fallback="gpt-3.5-turbo")
         self.record = self._config.getboolean("OPTION", "save_msg", fallback=True)
         self.interval = self._config.getint("OPTION", "interval", fallback=5)
